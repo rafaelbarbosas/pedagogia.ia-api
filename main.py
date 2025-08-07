b58b47b51f1bd8ddc50b4a02252527da3bc98310
rafaelbarbosas <rafael.1940.b@gmail.com>
2025-08-07 20:08:09 -0300

    Atualizada propriedade Allow-Origins

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # TODO: limitar
+    allow_origins=[os.getenv("ALLOW_ORIGINS")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
